41bbc0466571184bb62eb3f61cd8aaccdb99b4a4
Kseniia Dzhigun <kseniia.balandina@gmail.com>
2023-05-13 15:30:55 +0200

    fix: logger middleware, customised middleware separated

--- src/store/store.js
+++ src/store/store.js
@@ -1,27 +1,14 @@
 import { compose, applyMiddleware } from "redux";
-import { legacy_createStore as createStore} from 'redux';
+import { legacy_createStore as createStore } from 'redux';
 
 import { persistStore, persistReducer } from "redux-persist";
 
 import storage from "redux-persist/lib/storage";
 
-// import logger  from "redux-logger";
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-        return next(action);
-    }
-
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-
-    next(action);
-
-    console.log('next state: ', store.getState());
-}
 
 const persistConfig = {
     key: 'root',
@@ -31,10 +18,21 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+//If redux devtools exist then use composeEnhancer if not just normal compose
+const composeEnhancer =
+    (process.env.NODE_ENV !== 'production' &&
+        window &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
 
-export const store = createStore(persistedReducer, undefined, composedEnhancers);
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+    persistedReducer,
+    undefined,
+    composedEnhancers
+);
 
 export const persistor = persistStore(store);
